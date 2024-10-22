ab1b595a7004ecb647be896d07875446dbcb1f49
unknown <ghazalshabani.gca@gmail.com>
2024-10-22 10:37:22 -0700

    Exceptions handled.

--- environment.py
+++ environment.py
@@ -17,49 +17,47 @@ class TankwarsEnv(gym.Env):
             "fire": OneOf(True, False),
         })
 
-        def get_state(self, player_id):
-            self.client.request_observation(player_id, self.client.ObservationKind.IMAGE)
-            return self.client.player_state(player_id)
+    def get_state(self, player_id):
+        self.client.request_observation(player_id, self.client.ObservationKind.IMAGE)
+        return self.client.player_state(player_id)
 
-        def get_info(self):
-            pass
+    def get_info(self):
+        return {}
 
-        def reset(self, seed=None):
-            # super().reset(seed=seed)
+    def reset(self, seed=None):
+        # super().reset(seed=seed)
 
-            print("Requesting a player to be controlled")
-            player_id = client.get_player()
+        player_id = client.get_player()
 
-            if player_id is None:
-                return print("Failed to receive a player to control, quitting!")
+        if player_id is None:
+            raise Exception("Failed to receive a player to control, quitting!")
 
-            # 2. Subscribe to images and rewards for the spawned player
-            print("Subscribing to player")
-            client.subscribe_to_observation(player_id, self.client.ObservationKind.IMAGE, cooldown=0.1)
-            client.subscribe_to_observation(player_id, self.client.ObservationKind.REWARDS, cooldown=0.1)
+        # 2. Subscribe to images and rewards for the spawned player
+        client.subscribe_to_observation(player_id, self.client.ObservationKind.IMAGE, cooldown=0.1)
+        client.subscribe_to_observation(player_id, self.client.ObservationKind.REWARDS, cooldown=0.1)
 
-            return player_id
+        return player_id
 
-        def step(self, player_id, controls):
-            self.client.send_controls(player_id, controls)
-            player_state = self.get_state(player_id)
-            reward = player_state.pop("reward", 0.0)
-            terminated = False
-            truncated = False
-            info = self.get_info()
+    def step(self, player_id, controls):
+        self.client.send_controls(player_id, controls)
+        player_state = self.get_state(player_id)
+        reward = player_state.pop("reward", 0.0)
+        terminated = False
+        truncated = False
+        info = self.get_info()
 
-            return player_state, reward, terminated, truncated, info
+        return player_state, reward, terminated, truncated, info
 
-        def render(self, player_id, player_state):
-            image = player_state.get("image")
-            if image is not None:
-                cv2.imshow(f"Player #{player_id:160x}", image)
-                cv2.waitKey(1)  # Refresh the window to update the image
+    def render(self, player_id, player_state):
+        image = player_state.get("image")
 
-            else:
-                print("Player image was not set, skipping")
+        if image is None:
+            image = cv2.imread('./no_img.jpg')
 
-        gym.register(
-            id="gymnasium_env/Tankwars-v0",
-            entry_point=TankwarsEnv,
-        )
+        cv2.imshow(f"Player #{player_id:160x}", image)
+        cv2.waitKey(1)
+
+    gym.register(
+        id="gymnasium_env/Tankwars-v0",
+        entry_point=TankwarsEnv,
+    )
